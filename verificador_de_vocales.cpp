7a6d988e4cdce572f2327bec3df67fce2c36c45f
sebastiancardo <53200453+sebastiancardo@users.noreply.github.com>
2019-07-28 16:07:40 -0500

    Update verificador_de_vocales.cpp

--- verificador_de_vocales.cpp
+++ verificador_de_vocales.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int main()
-{
-  string 
-
-
-
-
+int main(){
+  char palabra[40];
+    int i,c=0;
+    cout<<"por favor ingrese una palabra: ";gets(palabra);
+    for (i=0;i<40;i++){
+        switch (palabra[i]){
+        case 'a':c=c+1;
+            break;
+        case 'e':c=c+1;
+            break;
+        case 'i':c=c+1;
+            break;
+        case 'o':c=c+1;
+            break;
+        case 'u':c=c+1;
+            break;
+        }
+    }
+    cout<<"el numero de vocales encontradas son:"<<c<<endl;
 }
