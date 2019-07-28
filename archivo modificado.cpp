c4ae1f37128afa88091beb79ab78115a933178f2
sebastiancardo <53200453+sebastiancardo@users.noreply.github.com>
2019-07-28 15:32:55 -0500

    Update archivo modificado.cpp

--- archivo modificado.cpp
+++ archivo modificado.cpp
@@ -1,15 +1,19 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-template <typename T> 
-T mayor(T x, T y) 
-{ 
-   return (x > y)? x: y; 
-} 
+template <typename T>
+
+T mayor(T x, T y)
+{
+   return (x > y)? x: y;
+}
 
 int main()
 {
-  cout<<"Esta aplicacion tiene una funcion para saber el mayor entre dos datos"<<endl;
-  return 0;
+    cout<<"Esta aplicacion tiene una funcion para saber el mayor entre dos datos"<<endl;
+    cout<< mayor(3,7) << endl;
+    cout<< mayor (4.0,9.8)<<endl;
+
+   return 0;
 }
